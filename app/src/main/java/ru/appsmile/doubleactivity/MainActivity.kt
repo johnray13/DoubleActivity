ea556b18a49ac633b46d7fd6cfbb0fb2c44fa6e9
johnray13 <144929963+johnray13@users.noreply.github.com>
2024-04-16 00:00:55 +0500

    DoubleActivity Final Commit

---
+++ app/src/main/java/ru/appsmile/doubleactivity/MainActivity.kt
@@ -0,0 +1,41 @@
+package ru.appsmile.doubleactivity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val passwordEditText1 = findViewById<EditText>(R.id.passwordEditText1)
+        val passwordEditText2 = findViewById<EditText>(R.id.passwordEditText2)
+        val submitButton = findViewById<Button>(R.id.submitButton)
+
+        submitButton.setOnClickListener{
+            val password1 = passwordEditText1.text.toString()
+            val password2 = passwordEditText2.text.toString()
+
+            if (password1 == password2) {
+                val intent = Intent(this, SecondActivity::class.java)
+                startActivity(intent)
+            } else {
+                    Toast.makeText(this, "Пароль не правильный или короткий", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+
+
+}
+
